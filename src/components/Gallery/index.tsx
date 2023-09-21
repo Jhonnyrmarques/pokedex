1547c8c2facaf26b7f4f401a2ea7ad776344a373
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-21 17:53:02 -0300

    feat: create Gallery component

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,43 @@
+import { useContext } from 'react'
+import { PokemonsContext } from '../../contexts/PokemonsContext'
+
+import * as S from './styles'
+import { Link } from 'react-router-dom'
+
+export function Gallery() {
+  const { pokemons } = useContext(PokemonsContext)
+
+  function getPokemonId(id: number) {
+    localStorage.setItem('pokemonId', id.toString())
+  }
+
+  return (
+    <S.Container>
+      {pokemons.map((item) => {
+        return (
+          <S.PokemonCard key={item.id} onClick={() => getPokemonId(item.id)}>
+            <Link to="/details">
+              <S.Gallery>
+                <img src={item.images} alt="" />
+                <span className="number">
+                  Nº {item.id.toString().padStart(4, '0')}
+                </span>
+                <h3>{item.name}</h3>
+
+                <div>
+                  {item.types.map((item, i) => {
+                    return (
+                      <S.PokemonsTypes variant={item.type.name} key={i}>
+                        {item.type.name}
+                      </S.PokemonsTypes>
+                    )
+                  })}
+                </div>
+              </S.Gallery>
+            </Link>
+          </S.PokemonCard>
+        )
+      })}
+    </S.Container>
+  )
+}
