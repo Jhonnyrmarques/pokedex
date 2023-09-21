167cec88decdbc4489a7fea0e68b262a00fe68c3
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-21 17:21:49 -0300

    feat: create colors theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,25 @@
+export const defaultTheme = {
+  white: '#ffffff',
+
+  'background-body': '#F15B6C',
+  'background-card': '#FEF9F3',
+
+  'search-icon': '#F15B6C',
+
+  'gray-200': '#E5E7EB',
+  'gray-300': '#D1D5DB',
+  'gray-400': '#9CA3AF',
+  'gray-500': '#6b7280',
+  'gray-900': '#111827',
+
+  'green-400': '#4ade80',
+  'green-500': '#22c55e',
+
+  'purple-500': '#a855f7',
+
+  'orange-500': '#f97316',
+
+  'blue-500': '#3b82f6',
+
+  'yellow-400': '#facc15',
+} as const
