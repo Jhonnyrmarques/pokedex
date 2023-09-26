fef279725afe607363375d6c7e1bbd813d25c3ac
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-25 21:36:37 -0300

    refactor: Header component removed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,10 @@
 import { Container } from '../../components/Container'
 import { Gallery } from '../../components/Gallery'
-import { Header } from '../../components/Header'
 import { PokemonsProvider } from '../../contexts/PokemonsContext'
 
 export function Home() {
   return (
     <Container>
-      <Header />
       <PokemonsProvider>
         <Gallery />
       </PokemonsProvider>
